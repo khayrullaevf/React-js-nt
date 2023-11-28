91ac79eff98a74fb7fcaab0b8560e49deabea177
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-11-28 09:42:04 +0500

    Lesson-3

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,17 +1,15 @@
-import Navbar from './components/homework/Navbar.jsx';
+
 import './App.css';
-import Modal from './components/homework/Modal.jsx';
-import RandomImgs from './components/homework/RandomImgs.jsx';
+import HomePage from './components/HomePage.jsx';
 
 
 function App() {
   return (
     <div className="App">
-    /<Navbar/>
-    <Modal/>
-    <RandomImgs/>
+    <HomePage/>
     </div>
   );
 }
 
 export default App;
+
