944eb725c28d5811bd2e5e6025c0dd821bde1b98
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-11-29 08:52:56 +0500

    Lesson-4 starting

--- my_app/src/pages/Homepage.jsx
+++ my_app/src/pages/Homepage.jsx
@@ -4,7 +4,8 @@ import { Fragment } from 'react'
 export const Homepage = () => {
   return (
     <Fragment>
-        <h1 className='text-center'>hello world</h1>
+      <h1 className="text-center">hello world</h1>
+      <h1 className="text-center">hello world</h1>
     </Fragment>
-  )
+  );
 }
