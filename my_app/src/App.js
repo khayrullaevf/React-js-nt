b6e70aa61c4b2df84deb72d20ebb3d9845aa5038
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-12-08 11:37:46 +0500

    Lesson-9

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,18 +1,51 @@
-
-
+import { useEffect } from "react";
+import { useMemo, useState } from "react";
+import List from "./components/List";
+import { useCallback } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
 
+  useEffect(() => {
+    console.log("theme changed");
+  }, [dark]);
 
-  const isLoggedIn=localStorage.getItem('email')&&localStorage.getItem('password')
-  console.log(isLoggedIn);
-  
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyle = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+  const getItems = useCallback(() => {
+    console.log("callback is working");
+    return [number, number + 1, number + 2];
+  }, [number]);
 
   return (
     <>
-     <h1>Lesson-9</h1>
+      <h1>Lesson-9</h1>
+
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <h2>{doubleNumber}</h2>
+      <List getItems={getItems} />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change theme
+      </button>
+      <div style={themeStyle}>Hello world</div>
     </>
   );
+}
+
+function slowFunction(num) {
+  console.log("slowFunction is working");
+  for (let index = 0; index < 1000000; index++) {}
+  return num * 2;
 }
 
 export default App;
---
+++ my_app/src/components/Counter.jsx
@@ -0,0 +1,41 @@
+import React, { useReducer } from 'react'
+
+
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return { count: state.count + 1 };
+      case "DECREMENT":
+        return { count: state.count - 1 };
+
+      default:
+        break;
+    }
+  };
+
+
+
+
+
+
+
+
+
+const Counter = () => {
+
+    const [state,dispatch]=useReducer(reducer,{count:0})
+  
+
+
+
+  return (
+    <div>
+      <h1>Count:{state.count}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+    </div>
+  );
+}
+
+export default Counter
